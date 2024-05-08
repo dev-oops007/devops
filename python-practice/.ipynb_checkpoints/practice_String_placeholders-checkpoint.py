ab44d894137b2daff77edecffb7ebd1a28633076
dev-oops007 <tomardev007@gmail.com>
2024-05-08 14:48:38 -0400

    adding string-methods with examples

---
+++ python-practice/.ipynb_checkpoints/practice_String_placeholders-checkpoint.py
@@ -0,0 +1,9 @@
+print ('Helo, What is your name?')
+name = input()
+print ('Nice to meet you '+ name)
+print('your name length is : ', len(name))
+print('What is your age ? ')
+age = int(input()) + 1
+print('your age will be '+ str(age) +' next year.')
+#F string place holder
+print (f'your age will be {age} next year.')
